f78e7e346f6b3655eb8b0d3acc85d06497fb8b63
Colton Buchanan <coltonbuchanan@email.arizona.edu>
2018-12-03 20:07:20 -0700

    Floor Destroyer

---
+++ Jump/Assets/Scenes/Scripts/FloorDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorDestroyer : MonoBehaviour {
+
+    public GameObject floorDestroyPoint;
+
+	// Use this for initialization
+	void Start () {
+        floorDestroyPoint = GameObject.Find("FloorDestroyPoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(transform.position.x < floorDestroyPoint.transform.position.x)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
