2a69bd9f51beab24d1e9105b52501f5e322b6ac0
Colton Buchanan <coltonbuchanan@email.arizona.edu>
2018-12-03 19:50:52 -0700

    Floor Generator

---
+++ Jump/Assets/Scenes/Scripts/FloorGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorGenerator : MonoBehaviour {
+
+    public GameObject floor;
+    public Transform spawnPoint;
+    public float distance;
+
+    private float floorWidth;
+
+	// Use this for initialization
+	void Start () {
+        floorWidth = floor.GetComponent<BoxCollider2D>().size.x;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(transform.position.x < spawnPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + floorWidth + distance, transform.position.y, transform.position.z);
+
+            Instantiate(floor, transform.position, transform.rotation);
+        }
+		
+	}
+}
